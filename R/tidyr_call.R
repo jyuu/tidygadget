f16636a35b61df92433589b39a891aeada28f860
jyuu <jyu21@ncsu.edu>
2019-08-20 13:35:19 -0400

    New tidyr call for pivot_long

---
+++ R/tidyr_call.R
@@ -0,0 +1,31 @@
+#' @importFrom rlang syms sym
+tidyr_call <- function(data = NULL,
+                       targets_fix = NULL,
+                       targets_pivot = NULL,
+                       settings = NULL) {
+  if (is.null(data)) {
+    return("")
+  }
+
+  data <- sym(data)
+
+  targets_fix <- dropNulls(targets_fix)
+  targets_pivot <- dropNulls(targets_pivot)
+
+  cols <- expr(-c(!!!syms(targets_fix)))
+  if (settings$type == "longer") {
+    settings <- syms(settings)
+    call <- expr(
+      pivot_longer(
+        data = !!data,
+        cols = !!cols,
+        names_to = paste0(expr(!!settings$names_column)),
+        values_to = paste0(expr(!!settings$values_column))
+      )
+    )
+  } else {
+    call <- "hello!"
+  }
+
+  call
+}
